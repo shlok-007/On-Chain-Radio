1bbab23afffe96c499acddcf99dce7d5363a88b4
anshg03 <99557054+anshg03@users.noreply.github.com>
2023-12-11 14:23:32 +0530

    profile image upload

--- client/src/utils/getUserAccount.ts
+++ client/src/utils/getUserAccount.ts
@@ -13,6 +13,7 @@ export default async function getUserAccount(address: string | null) {
         address,
         `${process.env.REACT_APP_MODULE_ADDR_TEST}::user::Account`,
         );
+        console.log(accountResource);
         let userAccount = {
         wallet_address: (accountResource as any).data.wallet_address,
         name: (accountResource as any).data.name,
@@ -21,6 +22,7 @@ export default async function getUserAccount(address: string | null) {
             location: (accountResource as any).data.bio.location,
             profession: (accountResource as any).data.bio.profession,
             about: (accountResource as any).data.bio.about,
+            profile_img_hash :(accountResource as any).data.bio.profile_img_hash,
         },
         premium: (accountResource as any).data.premium,
         subscription_expiry: (accountResource as any).data.subscription_expiry,
